5339b17f77b006d31b8423bf9874f1939a8b3e56
Fares Schulz <fares.schulz@miethke.com>
2023-07-16 13:18:56 +0200

    Workflow update

--- test/main/benchmark_Main.cpp
+++ test/main/benchmark_Main.cpp
@@ -1,7 +1,6 @@
 #include <benchmark/benchmark.h>
 #include "../SeamLess_Main/source/PluginEditor.h"
 
-
 static void BM_processor_main(benchmark::State& state) {
   // Perform setup here
     auto gui = juce::ScopedJuceInitialiser_GUI {};
@@ -13,16 +12,16 @@ static void BM_processor_main(benchmark::State& state) {
 // Register the function as a benchmark
 BENCHMARK(BM_processor_main);
 
-static void BM_editor_main(benchmark::State& state) {
-  // Perform setup here
-    auto gui = juce::ScopedJuceInitialiser_GUI {};
-    AudioPluginAudioProcessor plugin;
-  for (auto _ : state) {
-    // This code gets timed
-    auto editor = plugin.createEditor();
-    plugin.editorBeingDeleted (editor);
-    delete editor;
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(BM_editor_main);
+// static void BM_editor_main(benchmark::State& state) {
+//   // Perform setup here
+//     auto gui = juce::ScopedJuceInitialiser_GUI {};
+//     AudioPluginAudioProcessor plugin;
+//   for (auto _ : state) {
+//     // This code gets timed
+//     auto editor = plugin.createEditor();
+//     plugin.editorBeingDeleted (editor);
+//     delete editor;
+//   }
+// }
+// // Register the function as a benchmark
+// BENCHMARK(BM_editor_main);

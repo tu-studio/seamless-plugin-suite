faf27b7c3d68b1fa094e9c414fad2c9b88244f28
Fares Schulz <schulzfares@gmail.com>
2023-09-13 15:55:51 +0200

    OSC Adress now changeable

---
+++ SeamLess_Main/source/ui/oscConnectionBox/oscSendAdressSelector/OSCSendAdressSelector.h
@@ -0,0 +1,39 @@
+/*
+=====================================================================
+
+OSCSendAdressSelector.h
+Created: 13 Sep 2021
+Author:  Fares Schulz
+
+=====================================================================
+*/
+
+#ifndef OSCSendAdressSelector_h
+#define OSCSendAdressSelector_h
+
+#include <JuceHeader.h>
+#include <SeamLess.h>
+#include <PluginParameters.h>
+
+class OSCSendAdressSelector : public juce::Component {
+public:
+    OSCSendAdressSelector(juce::AudioProcessorValueTreeState &apvts);
+    ~OSCSendAdressSelector() override;
+    
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+
+private:
+    void oscSendAdressTextChanged();
+
+private:
+    juce::AudioProcessorValueTreeState& apvts;
+
+    juce::Label descLabel;
+    juce::Label oscSendAdressText;
+    juce::Label connectionStatusLabel;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OSCSendAdressSelector)
+};
+
+#endif /* OSCSendAdressSelector_h */

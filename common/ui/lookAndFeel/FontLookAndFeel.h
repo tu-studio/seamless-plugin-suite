5d1a89eabae331636bae9e0a2f8fc5a1d0af9ef0
Max Weidauer <max+git@weidauer.art>
2026-03-31 17:40:25 +0200

    updated JUCE version

--- common/ui/lookAndFeel/FontLookAndFeel.h
+++ common/ui/lookAndFeel/FontLookAndFeel.h
@@ -11,19 +11,19 @@ public:
     const juce::Font getCustomFont()
     {
         auto typeface = juce::Typeface::createSystemTypefaceFor (BinaryData::FiraCodeNerdFontBold_ttf, BinaryData::FiraCodeNerdFontBold_ttfSize);
-        return juce::Font (typeface);
+        return juce::Font (juce::FontOptions(typeface));
     }
 
     const juce::Font getCustomFontBold()
     {
         auto typeface = juce::Typeface::createSystemTypefaceFor (BinaryData::FiraCodeNerdFontMedium_ttf, BinaryData::FiraCodeNerdFontMedium_ttfSize);
-        return juce::Font (typeface);
+        return juce::Font (juce::FontOptions(typeface));
     }
 
     const juce::Font getCustomFontMedium()
     {
         auto typeface = juce::Typeface::createSystemTypefaceFor (BinaryData::FiraCodeNerdFontRegular_ttf, BinaryData::FiraCodeNerdFontRegular_ttfSize);
-        return juce::Font (typeface);
+        return juce::Font (juce::FontOptions(typeface));
     }
 
     juce::Typeface::Ptr getTypefaceForFont (const juce::Font& f) override
